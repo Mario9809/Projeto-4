81c21587b306c3f085ad851da9e2f98552b512cb
Cecília Martins De Oliveira <135181058+oliivercm@users.noreply.github.com>
2024-05-13 22:37:56 +0000

    Implementação da função extrato, implementação do salvamento em arquivo binário, correção das saidas de depósito e transferência, e correção nas funções novo cliente e débito.

--- main.c
+++ main.c
@@ -1,19 +1,11 @@
 #include "sistema.h"
 #include <stdio.h>
-
-
+#include <stdlib.h>
 
 int main() {
-
-
     Cliente cliente[TOTAL];
-    //Cria uma array do tipo Cliente com o nome cliente com o máximo de elementos definido pelo total
     int pos;
-    //declara uma variável para contador com o nome pos
     ERROS erro = carregar(cliente, &pos);
-    // declara uma variável do tipo ERROS com o nome erro, utilizada a função carregar para acessar um elemento da array cliente na posição dada pelo ponteiro.
-
-  
     if (erro != OK)
         pos = 0;
 
@@ -25,70 +17,46 @@ int main() {
         printf("3 - Listar Clientes\n");
         printf("4 - Débito\n");
         printf("5 - Depósito\n");
-        printf("6 - Exrato\n");
+        printf("6 - Extrato\n");
         printf("7 - Transferência entre contas\n");
         printf("0 - Sair\n");
         printf("Escolha uma opcao: ");
         scanf("%d", &opcao);
 
-
-      switch (opcao) {
-
-          case 1:
-
-              Novo_cliente(cliente, &pos);
-
-              break;
-
-          case 2:
-
-              Apagar_cliente(cliente, &pos);
-
-              break;
-
-          case 3:
-
-              Listar_clientes(cliente, &pos);
-
-              break;
-
-          case 4:
-
-              Debito(cliente, &pos);
-
-              break;
-
-          case 5:
-
-              Deposito(cliente, &pos);
-
-              break;
-
-          case 6:
-
-              Extrato(cliente, &pos);
-
-              break;     
-
-          case 7:
-
-              Transferencia_entre_contas(cliente, &pos);
-
-              break;   
-
-          case 0:
-
-              printf("Saindo...\n");
-
-              break;
-
-          default:
-
-              printf("Escolha inválida.\n");
-
-      }
-
+        switch (opcao) {
+            case 1:
+                Novo_cliente(cliente, &pos);
+                break;
+            case 2:
+                Apagar_cliente(cliente, &pos);
+                break;
+            case 3:
+                Listar_clientes(cliente, &pos);
+                break;
+            case 4:
+                Debito(cliente, &pos);
+                break;
+            case 5:
+                Deposito(cliente, &pos);
+                break;
+            case 6:
+                Extrato(cliente, &pos);
+                break;
+            case 7:
+                Transferencia_entre_contas(cliente, &pos);
+                break;
+            case 0:
+                printf("Saindo...\n");
+                break;
+            default:
+                printf("Escolha inválida.\n");
+        }
     } while (opcao != 0);
+
+    // Salvando os dados no arquivo antes de sair
+    erro = salvar(cliente, pos);
+    if (erro != OK)
+        printf("Erro ao salvar os dados no arquivo.\n");
 
     return 0;
 }
--- sistema.h
+++ sistema.h
@@ -7,20 +7,23 @@
 #define TOTAL 1000
 #define TAM_NOME 1000
 #define TAM_SOBRENOME 1000
-#define TAM_tipodeconta 300
+#define TAM_TIPO_CONTA 300
 #define TAM_CPF 300
+#define MAX_OPERACOES 100
 
 typedef struct {
     char Nome[TAM_NOME];
     char Sobrenome[TAM_SOBRENOME];
     int CPF;
-    char Tipo_de_conta[TAM_tipodeconta];
+    char Tipo_de_conta[TAM_TIPO_CONTA];
     float Saldo_inicial;
-    float Saldo_atual; // Alterado para float para permitir valores decimais
-    char Senha[16]; // Alterado para uma string para armazenar a senha
+    float Saldo_atual;
+    char Senha[16];
+    int num_operacoes;  // Número de operações realizadas
+    float operacoes[MAX_OPERACOES];  // Registro das operações
 } Cliente;
 
-typedef enum {OK, MAX_CLIENTES, SEM_CLIENTES, NAO_ENCONTRADO, SENHA, SALDO_INSUFICIENTE} ERROS;
+typedef enum {OK, MAX_CLIENTES, SEM_CLIENTES, NAO_ENCONTRADO, SENHA, SALDO_INSUFICIENTE, ERRO_ABRIR_ARQUIVO, ERRO_ESCREVER_ARQUIVO} ERROS;
 
 void clearBuffer();
 
@@ -39,5 +42,7 @@ ERROS Extrato(Cliente cliente[], int *pos);
 ERROS Transferencia_entre_contas(Cliente cliente[], int *pos);
 
 ERROS carregar(Cliente cliente[], int *pos);
+
+ERROS salvar(Cliente cliente[], int pos);
 
 #endif /* SISTEMA_H */
